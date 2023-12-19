06e510d62c6a4d7a4977cf3ea2fbfbfb8cdad6ac
Jared Collier <collierjared@gmail.com>
2023-12-19 15:36:57 -0700

    App component loops over fake pokemon data to render Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+import { useState } from 'react'
 import Card from './Card.jsx'
 import './App.css'
 
+//we are going to pretend that this is data we got from an API
+const theList = ['raichu', 'voltorb', 'Rhydon']
+
 function App() {
+
+  const [pokemonList, setPokemonList] = useState(theList)
 
   return (
     <div>
       <h1>Here are some pokemon!</h1>
-      <Card pokemon={'Pikachu'}/>
-      <Card pokemon={'Raichu'}/>
-      <Card pokemon={'Voltorb'}/>
+      { 
+        pokemonList.map((element, index, array) => {
+          return <Card pokemon={element}/>
+        })
+      }
     </div>
   )
 }
